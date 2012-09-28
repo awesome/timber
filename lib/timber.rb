d65e0cb127471b8ae027d84d46bdb23aea4695fe
Cory Schires <coryschires@gmail.com>
2012-09-28 17:08:39 -0500

    Added new method to trigger previously subscribed events.

--- lib/timber.rb
+++ lib/timber.rb
@@ -21,6 +21,27 @@ module Timber
     end
   end
 
+  # Allows you to trigger a subscribe method without having to actually move through the controller. Can
+  # help avoid duplicating the logic used to create a given timber activity.
+  #
+  # controller_action - Controller name and action name in shorthand notation (e.g. 'posts#new')
+  # current_user      - The current user instance (an object) or id (an integer)
+  # params            - Any additional params which you'd like added to the payload.
+  #
+  # Examples
+  #
+  #   Timber.trigger("article#create", @user, { publication_id: 88 })
+  #
+  def self.trigger(controller_action, current_user, params)
+    controller, action = controller_action.split('#')
+    full_params = { controller: controller, action: action }.merge(params)
+    current_user_id = current_user.respond_to?(:id) ? current_user.id : current_user
+
+    ActiveSupport::Notifications.instrument("process_action.action_controller", {
+      current_user_id: current_user_id, params: full_params
+    })
+  end
+
   # Allow client apps to use +link_to+ within a +subscribe+ block.
   #
   def self.link_to(*args, &block)
