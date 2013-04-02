cc1cbeab84e36e8889d453aee77d83d1594120c7
Cory Schires <coryschires@gmail.com>
2013-04-02 15:10:38 -0500

    Update method missing path helper to look for both `_path` and `_url` methods.

--- lib/timber.rb
+++ lib/timber.rb
@@ -83,7 +83,7 @@ module Timber
   # `subscribe` block.
   #
   def self.method_missing(method, *args, &blk)
-    if method =~ /_path$/
+    if method =~ /_path$/ || method =~ /_url$/
       Rails.application.routes.url_helpers.send(method, *args, &blk)
     else
       super
